2ae329c7b37f3a4ae67455db97e74183cd06e955
Sutulov <is.gepich@gmail.com>
2021-12-02 15:28:57 +0200

    add getPrice method

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-import dotenv from "dotenv"
-dotenv.config()
-// import fetch from "node-fetch"
-import Binance from "node-binance-api"
-import express from "express"
-const SERVER_SUBMIT_ORDER_PORT = process.env.SERVER_SUBMIT_ORDER_PORT
-const app = express()
+import dotenv from "dotenv";
+import Binance from "node-binance-api";
+import express from "express";
+import {submitLog} from './loggingConf.js';
 
-import {submitLog, errorLog, debugLog, defaultLog} from './loggingConf.js'
+dotenv.config();
+const SERVER_SUBMIT_ORDER_PORT = process.env.SERVER_SUBMIT_ORDER_PORT;
+const app = express();
 
 const binance = new Binance().options({
     APIKEY: process.env.API_KEY,
@@ -16,55 +15,80 @@ const binance = new Binance().options({
 
 //
 app.post('/submit', (req, res) => {
-    let postBodyRequest = ''
+    let postBodyRequest = '';
     req.on('data', chunk => {
-        postBodyRequest += chunk.toString()
+        postBodyRequest += chunk.toString();
     });
 
+
     req.on('end', ()=>{
-        let params = JSON.parse(postBodyRequest)
-        submitLog.info("params: ", params )
-        let orderType = params.type
-        let pair = params.pair
+        let params = JSON.parse(postBodyRequest);
+        submitLog.info("params: ", params );
+        let orderType = params.type;
+        let pair = params.pair;
         let quantity = params.volume;
-        // let amount = params.volume
-        // let nonce = (Date.now() * 1000).toString()
-        // let body = {
-        //     type: orderType,
-        //     symbol: pair,
-        //     amount: amount
-        // }
-        // submitLog.info('body: ', body)
 
         // These orders will be executed at current market price.
-        if(orderType === "short"){
-            binance.marketSell(pair, quantity);
+        if(orderType === "short") {
+            binance.marketSell(pair, quantity, {type:'LIMIT'}, (error, response) => {
+                console.info("Market Buy response", response)
+                console.info("order id: " + response.orderId)
+            })
         }
-        if(orderType === "long"){
-            binance.marketBuy(pair, quantity);
+
+
+        if(orderType === "long") {
+            binance.marketBuy(pair, quantity)
+                .then(a => submitLog.info("HI"))
         }
-
-
-        // fetch(`https://api.bitfinex.com/${apiPathSubmit}`, {
-        //     method: 'POST',
-        //     body: JSON.stringify(body),
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'bfx-nonce': nonce,
-        //         'bfx-apikey': apiKey,
-        //         'bfx-signature': sig
-        //     }
-        // })
-        //     .then(res => res.json())
-        //     .then(json => res.end(Buffer.from(JSON.stringify(json)))) // !!!!Возращает
-        //     .then(json => debugLog.debug(json))
-        //     .catch(err => {
-        //         errorLog.error(err, "Response error")
-        //     })
-
+        // binance.buy(pair, quantity, 0, "MARKET")
     })
 })
-//
+
+app.get('/price', (req, res) => {
+    getLastPrice()
+        .then(async () => res.status(200).send(await getLastPrice()))
+        // .then(c => console.log(c))
+        // .then(res => console.log(res)
+
+
+    // req.on(getLastPrice())
+
+// .then(res => res.status(200).send("THEN"))
+//     res.status(200).send("getLastPrice()")
+
+
+    // Getting list of open orders
+    // binance.openOrders("ETHBTC", function(error, json) {
+        // console.log("openOrders()",json);
+    // });
+
+    // // .then(res =   > res.json())
+        // // .then(json => res.end(Buffer.from(JSON.stringify(json)))) // !!!!Возращает
+        // // .then(json => debugLog.debug(json))
+        // .catch(err => {
+        //     console.log(err, "Response error")
+        // })
+})
+
+/**
+ * @returns {JSON}
+ */
+async function getLastPrice(){
+    const a = await binance.futuresPrices()
+    return {
+        "BTCUSDT": a.BTCUSDT
+    }
+}
+
+/**
+ *
+ * @returns {String}
+ */
+// async function getLastPrice(){
+//     const a = await binance.futuresPrices()
+//     return a.BTCUSDT
+// }
 
 app.listen(SERVER_SUBMIT_ORDER_PORT,() => {
     console.log('Server has been started on port', + SERVER_SUBMIT_ORDER_PORT, '...')
